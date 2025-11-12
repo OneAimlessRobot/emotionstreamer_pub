5dd1b032c0f0e95f2d0393b043aaa2910696b96e
addysmagic <adri.campos.valente@gmail.com>
2025-11-12 23:21:15 +0000

    bhjgbhkjhkjhk

--- code/client/Sources/streamer_client.c
+++ code/client/Sources/streamer_client.c
@@ -424,10 +424,10 @@ static void* input_thread_func(void* args){
 
 		char input_buff[DEF_DATASIZE+1]={0};
 		if(stream_enable_ncurses){
-			scanf("%s",input_buff);
+			scanw("%s",input_buff);
 		}
 		else{
-			scanw("%s",input_buff);
+			scanf("%s",input_buff);
 		}switch(input_buff[0]){
 
 			case 'p':
