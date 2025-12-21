0ca837cf16b1e07b4efd43c3c048c7110bdfbc74
addysmagic <adri.campos.valente@gmail.com>
2025-12-21 16:49:35 +0000

    bhjgbhkjhkjhk

--- code/startup_tmux.sh
+++ code/startup_tmux.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
+directory="$(pwd)"
 
-directory=$(pwd)
+tmux new-session -d -s my_stuff "bash -lc 'cd \"$directory/port_mapper\" && ./port_mapper.exe; exec bash'"
 
-tmux new-sesstion -d -s my_stuff "pushd $directory && pushd port_mapper && ./*exe"
-tmux new-window -t my_stuff:2 "pushd $directory && pushd server && ./*exe"
-tmux new-window -t my_stuff:3 "pushd $directory && pushd client"
-tmux attack -t my_stuff
+tmux new-window -t my_stuff:2 "bash -lc 'cd \"$directory/server\" && ./server.exe; exec bash'"
+
+tmux new-window -t my_stuff:3 "bash -lc 'cd \"$directory/client\"; exec bash'"
+
+tmux attach -t my_stuff
