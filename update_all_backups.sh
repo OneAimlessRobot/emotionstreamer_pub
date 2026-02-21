0d51737f7da3fdaabd51f237e410ab82c3c17657
addysmagic <adri.campos.valente@gmail.com>
2026-02-21 00:29:23 +0000

    bhjgbhkjhkjhk

---
+++ update_all_backups.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+backup_one="/mnt/FASTstorage/FASTprogs"
+backup_two="/mnt/REBORN/FASTERprogs"
+backup_three="/mnt/SUPER_CAVALEIRO/progsBackup"
+
+pushd "${backup_one}/emotionstreamer/code" &&
+bash makeAll.sh &&
+popd &&
+bash updateRepoMasterBranch.sh &&
+rm -rf "${backup_one}/emotionstreamer_pub/code" &&
+rm -rf "${backup_one}/emotionstreamer_priv/code" &&
+rm -rf "${backup_two}/emotionstreamer_pub/code" &&
+rm -rf "${backup_two}/emotionstreamer_priv/code" &&
+rm -rf "${backup_two}/emotionstreamer/code" &&
+rm -rf "${backup_three}/emotionstreamer_pub/code" &&
+rm -rf "${backup_three}/emotionstreamer_priv/code" &&
+rm -rf "${backup_three}/emotionstreamer/code" &&
+cp -rf code "${backup_one}/emotionstreamer_pub" &&
+cp -rf code "${backup_one}/emotionstreamer_priv" &&
+cp -rf code "${backup_two}/emotionstreamer_pub" &&
+cp -rf code "${backup_two}/emotionstreamer_priv" &&
+cp -rf code "${backup_two}/emotionstreamer" &&
+cp -rf code "${backup_three}/emotionstreamer_pub" &&
+cp -rf code "${backup_three}/emotionstreamer_priv" &&
+cp -rf code "${backup_three}/emotionstreamer" &&
+pushd "${backup_one}/emotionstreamer_pub" &&
+bash updateRepoMasterBranch.sh &&
+popd
