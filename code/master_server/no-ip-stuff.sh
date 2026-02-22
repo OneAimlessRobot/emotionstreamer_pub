2c3f49d84c521b8265168641c3c9f4401491140d
addysmagic <adri.campos.valente@gmail.com>
2026-02-22 23:17:42 +0000

    bhjgbhkjhkjhk

--- code/master_server/no-ip-stuff.sh
+++ code/master_server/no-ip-stuff.sh
@@ -5,5 +5,7 @@
 #username/email: pzkcxhz@ddnskey.com
 #password: 5FGymDwo53ry
 #ddns key hostname: all.ddnskey.com
+#login email: shellshockedlena@gmail.com
+#password: Th3widowmaker
 noip-duc --check-interval 5m --username  pzkcxhz@ddnskey.com --password 5FGymDwo53ry --ip-method static:85.138.81.153 -g lenasattic.ddns.net --daemon-group lara_and_lenas_group
 
