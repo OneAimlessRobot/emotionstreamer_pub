2febea15f52682b5a11d10972ff07c9ae3672ff3
addysmagic <adri.campos.valente@gmail.com>
2026-02-24 16:31:49 +0000

    bhjgbhkjhkjhk

--- update_all_backups.sh
+++ update_all_backups.sh
@@ -4,6 +4,8 @@ backup_one="/mnt/FASTstorage/FASTprogs"
 backup_two="/mnt/REBORN/FASTERprogs"
 backup_three="/mnt/SUPER_CAVALEIRO/progsBackup"
 
+file_extensions="*.{md,sh,webp}"
+
 pushd "${backup_one}/emotionstreamer/code" &&
 bash makeAll.sh &&
 popd &&
@@ -16,21 +18,21 @@ rm -rf "${backup_two}/emotionstreamer/code" &&
 rm -rf "${backup_three}/emotionstreamer_pub/code" &&
 rm -rf "${backup_three}/emotionstreamer_priv/code" &&
 rm -rf "${backup_three}/emotionstreamer/code" &&
-cp -rf *.sh "${backup_one}/emotionstreamer_pub" &&
+cp -rf *.{md,sh,webp} "${backup_one}/emotionstreamer_pub" &&
 cp -rf code "${backup_one}/emotionstreamer_pub" &&
-cp -rf *.sh "${backup_one}/emotionstreamer_priv" &&
+cp -rf *.{md,sh,webp} "${backup_one}/emotionstreamer_priv" &&
 cp -rf code "${backup_one}/emotionstreamer_priv" &&
-cp -rf *.sh "${backup_two}/emotionstreamer_pub" &&
+cp -rf *.{md,sh,webp} "${backup_two}/emotionstreamer_pub" &&
 cp -rf code "${backup_two}/emotionstreamer_pub" &&
-cp -rf *.sh "${backup_two}/emotionstreamer_priv" &&
+cp -rf *.{md,sh,webp} "${backup_two}/emotionstreamer_priv" &&
 cp -rf code "${backup_two}/emotionstreamer_priv" &&
-cp -rf *.sh "${backup_two}/emotionstreamer" &&
+cp -rf *.{md,sh,webp} "${backup_two}/emotionstreamer" &&
 cp -rf code "${backup_two}/emotionstreamer" &&
-cp -rf *.sh "${backup_three}/emotionstreamer_pub" &&
+cp -rf *.{md,sh,webp} "${backup_three}/emotionstreamer_pub" &&
 cp -rf code "${backup_three}/emotionstreamer_pub" &&
-cp -rf *.sh "${backup_three}/emotionstreamer_priv" &&
+cp -rf *.{md,sh,webp} "${backup_three}/emotionstreamer_priv" &&
 cp -rf code "${backup_three}/emotionstreamer_priv" &&
-cp -rf *.sh "${backup_three}/emotionstreamer" &&
+cp -rf *.{md,sh,webp} "${backup_three}/emotionstreamer" &&
 cp -rf code "${backup_three}/emotionstreamer" &&
 pushd "${backup_one}/emotionstreamer_pub" &&
 bash updateRepoMasterBranch.sh &&
