8b79c2d9a4c3e3b59386cd922321531a5097658f
addysmagic <adri.campos.valente@gmail.com>
2026-02-24 16:16:29 +0000

    bhjgbhkjhkjhk

--- update_all_backups.sh
+++ update_all_backups.sh
@@ -16,13 +16,21 @@ rm -rf "${backup_two}/emotionstreamer/code" &&
 rm -rf "${backup_three}/emotionstreamer_pub/code" &&
 rm -rf "${backup_three}/emotionstreamer_priv/code" &&
 rm -rf "${backup_three}/emotionstreamer/code" &&
+cp -rf *.sh "${backup_one}/emotionstreamer_pub" &&
 cp -rf code "${backup_one}/emotionstreamer_pub" &&
+cp -rf *.sh "${backup_one}/emotionstreamer_priv" &&
 cp -rf code "${backup_one}/emotionstreamer_priv" &&
+cp -rf *.sh "${backup_two}/emotionstreamer_pub" &&
 cp -rf code "${backup_two}/emotionstreamer_pub" &&
+cp -rf *.sh "${backup_two}/emotionstreamer_priv" &&
 cp -rf code "${backup_two}/emotionstreamer_priv" &&
+cp -rf *.sh "${backup_two}/emotionstreamer" &&
 cp -rf code "${backup_two}/emotionstreamer" &&
+cp -rf *.sh "${backup_three}/emotionstreamer_pub" &&
 cp -rf code "${backup_three}/emotionstreamer_pub" &&
+cp -rf *.sh "${backup_three}/emotionstreamer_priv" &&
 cp -rf code "${backup_three}/emotionstreamer_priv" &&
+cp -rf *.sh "${backup_three}/emotionstreamer" &&
 cp -rf code "${backup_three}/emotionstreamer" &&
 pushd "${backup_one}/emotionstreamer_pub" &&
 bash updateRepoMasterBranch.sh &&
