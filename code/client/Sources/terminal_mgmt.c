eb7dd8bd20a2fca4f5ac9abc19b4d6919b7ac0e0
addysmagic <adri.campos.valente@gmail.com>
2026-02-27 19:59:07 +0000

    bhjgbhkjhkjhk

--- code/client/Sources/terminal_mgmt.c
+++ code/client/Sources/terminal_mgmt.c
@@ -208,6 +208,7 @@ setvbuf(stdout, NULL,_IONBF,0);
 setvbuf(stderr, NULL,_IONBF,0);
 fprintf(stderr,"Is stdin a tty? %d\nIs stdout a tty? %d\nWhat term are we in? %s\n\n",isatty(0),isatty(1),getenv("TERM"));
 setenv("TERM","xterm-256color",1);
+fprintf(stderr,"Is stdin a tty? %d\nIs stdout a tty? %d\nWhat term are we in [now]? %s\n\n",isatty(0),isatty(1),getenv("TERM"));
 //FILE* tty = fopen("/dev/tty","r+");
 //SCREEN*s=newterm(NULL,tty,tty);
 //set_term(s);
